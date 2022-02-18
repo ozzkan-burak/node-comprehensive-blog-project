4c8c66a2b37d6ba0233048366a1f0b6d7ded3834
Burak Özkan <burak.ozkan@path.com.tr>
2022-02-18 18:36:45 +0300

    episode1

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+const data = require ('../dummy/data');
+
 router.get('/', (req, res)=>{
-  res.send('TEST EDIYORUZ');
+  res.render('home', {data});
+});
+
+router.get('/about', (req, res)=>{
+  res.render('about');
+});
+
+router.get('/contact', (req, res)=>{
+  res.render('contact');
 })
+
 
 
 module.exports = router;
