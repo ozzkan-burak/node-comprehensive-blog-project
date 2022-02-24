13645303c4c3b417a77177011da99bcc62022654
Burak Özkan <burak.ozkan@path.com.tr>
2022-02-24 23:07:42 +0300

    episode4

---
+++ models/sitemodel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const siteSchema = new mongoose.Schema({
+  homeImage: {type: String, required: "Cannot be empty"},
+  aboutImage: {type: String, required: "Cannot be empty"},
+  aboutText: {type: String, required: "Cannot be empty"},
+  contactImage: {type: String, required: "Cannot be empty"},
+});
+
+module.exports = mongoose.model('Site', siteSchema);
